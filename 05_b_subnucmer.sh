495cc6a65f20edc5f67b9c82546979ce7bcf97c4
Lenax_D <122524295+XiaoyueLenax@users.noreply.github.com>
2024-01-31 18:19:39 +0100

    Update 05_b_subnucmer.sh

--- 05_b_subnucmer.sh
+++ 05_b_subnucmer.sh
@@ -15,26 +15,20 @@
 #2. assembly_name=flye;    assembly=${course_dir}/02_assembly/flye/assembly.fasta;     reference=${raw_data_dir}/Arabidopsis_thaliana.TAIR10.dna.toplevel.fa
 #3. assembly_name=compare; assembly=${course_dir}/02_assembly/canu/canu.contigs.fasta; reference=${course_dir}/02_assembly/flye/assembly.fasta
 
-#Add the modules
-    module add UHTS/Analysis/mummer/4.0.0beta1
+course_dir=/data/users/xdeng/assembly_course
+raw_data_dir=${course_dir}/RawData
+comparison_dir=${course_dir}/04_comparison
+nucmer_dir=${comparison_dir}/nucmer
+assembly_nucmer_dir=${nucmer_dir}/${assembly_name}
 
-#Specify name of assembly (!!!COMMENT OUT THE ONE YOU ARE NOT USING!!!)
-    assembly_name=canu
-    # assembly_name=flye
-    # assembly_name=compare
+module add UHTS/Analysis/mummer/4.0.0beta1
 
-#Specify directory structure and create them
-    course_dir=/data/users/srasch/assembly_course
-        raw_data_dir=${course_dir}/RawData
-        comparison_dir=${course_dir}/04_comparison
-            nucmer_dir=${comparison_dir}/nucmer
-                assembly_nucmer_dir=${nucmer_dir}/${assembly_name}
-    
-    mkdir ${comparison_dir}
-    mkdir ${nucmer_dir}
-    mkdir ${assembly_nucmer_dir}
+#Task ID
+assembly_name=canu
+# assembly_name=flye
+# assembly_name=compare
 
-#Specify the assembly to use (!!!COMMENT OUT THE ONE YOU ARE NOT USING!!!)
+#Input Files
     assembly=${course_dir}/02_assembly/canu/canu.contigs.fasta
     # assembly=${course_dir}/02_assembly/flye/assembly.fasta
 
@@ -47,7 +41,10 @@
 
 #Run nucmer to compare assemblies
     nucmer -b 1000 -c 1000 -p ${assembly_name} ${reference} ${assembly}
-        #Options entered here are:
-            #"-b 1000": Distance an alignment extension will attempt to extend poor scoring regions before giving up (default 200)
-            #"-c 1000": Minimum cluster length
-            #"-p": Set the output file prefix
+# Options explained:
+    #- `nucmer`: Invokes the NUCmer tool from MUMmer for aligning genomes.
+    #- `-b 1000`: Sets the maximum gap between adjacent matches in a cluster to 1000 base pairs.
+    #- `-c 1000`: Specifies the minimum cluster length as 1000 base pairs.
+    #- `-p ${assembly_name}`: Uses the `${assembly_name}` variable as the prefix for output files.
+    #- `${reference}`: Path to the reference genome file.
+    #- `${assembly}`: Path to the query genome (assembly) file to be aligned.
