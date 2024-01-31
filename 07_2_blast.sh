e554842bae0b10d19b61a0c258823aa4348f9247
Lenax_D <122524295+XiaoyueLenax@users.noreply.github.com>
2024-01-31 18:45:27 +0100

    Update 07_2_blast.sh

--- 07_2_blast.sh
+++ 07_2_blast.sh
@@ -18,5 +18,18 @@ COURSE_DIR=/data/courses/assembly-annotation-course
 OUT_DIR=/data/users/xdeng/assembly_annotation_course/09_Annotation_Control/BLAST
 cd ${OUT_DIR}/BLAST_DB
 
+# Creates a BLAST database from protein sequences.
 makeblastdb -in ${COURSE_DIR}/CDS_annotation/uniprot-plant_reviewed.fasta -dbtype prot -out ${OUT_DIR}/BLAST_DB/uniprot_plant_reviewed
+# Options explained
+  #-in: Specifies the input FASTA file of protein sequences.
+  #-dbtype prot: Sets the database type to protein.
+  #-out: Defines the output database name and location.
+
+#Runs protein-protein BLAST searches. 
 blastp -query ${INPUT_FILE} -db ${OUT_DIR}/BLAST_DB/uniprot_plant_reviewed -num_threads 30 -outfmt 6 -evalue 1e-10 -out ${OUT_DIR}/blastp.out
+    #-query: Input file with protein sequences to search.
+    #-db: Specifies the BLAST database for searching.
+    #-num_threads 30: Uses 30 CPU cores for the search.
+    #-outfmt 6: Sets output format to tabular.
+    #-evalue 1e-10: Filters hits with E-values above 1e-10.
+    #-out: Names and specifies location for the output file.
