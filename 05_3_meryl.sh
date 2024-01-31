43e1f203e2c82be5dbcf350182a6a99456dc459a
Lenax_D <122524295+XiaoyueLenax@users.noreply.github.com>
2024-01-31 17:53:20 +0100

    Update 05_3_meryl.sh

--- 05_3_meryl.sh
+++ 05_3_meryl.sh
@@ -10,23 +10,13 @@
 #SBATCH --error=/data/users/xdeng/assembly_annotation_course/outputs_errors/error_meryl_w_%j.e
 #SBATCH --partition=pall
 
-#Add the modules
-    module add UHTS/Assembler/canu/2.1.1
-
-#Specify directory structure and create them
-    course_dir=/data/users/xdeng/assembly_annotation_course
-        polish_evaluation_dir=${course_dir}/polish_eval
-            evaulation_dir=${polish_evaluation_dir}/eval
-                meryl_dir=${evaulation_dir}/meryl
+module add UHTS/Assembler/canu/2.1.1
+course_dir=/data/users/xdeng/assembly_annotation_course
+polish_evaluation_dir=${course_dir}/polish_eval
+evaulation_dir=${polish_evaluation_dir}/eval
+meryl_dir=${evaulation_dir}/meryl
     
-    #  mkdir ${meryl_dir}
-
-
-
-    data_dir=/data/users/xdeng/assembly_annotation_course/raw_data/Illumina
-
+data_dir=/data/users/xdeng/assembly_annotation_course/raw_data/Illumina
 meryl k=19 count output $SCRATCH/read_1.meryl ${data_dir}/*1.fastq.gz
-
 meryl k=19 count output $SCRATCH/read_2.meryl ${data_dir}/*2.fastq.gz
-
 meryl union-sum output ${meryl_dir}/genome.meryl $SCRATCH/read*.meryl
