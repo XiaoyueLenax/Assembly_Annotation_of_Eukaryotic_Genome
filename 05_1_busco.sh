cbd92eda0bf76b2f028e0cbfa3f2086d7dffa5b6
Lenax_D <122524295+XiaoyueLenax@users.noreply.github.com>
2024-01-31 16:02:02 +0100

    Update 05_1_busco.sh

--- 05_1_busco.sh
+++ 05_1_busco.sh
@@ -13,31 +13,28 @@
 #Add the modules
     module add UHTS/Analysis/busco/4.1.4
 
-#Specify name of assembly (!!!COMMENT OUT THE ONE YOU ARE NOT USING!!!)
+#task name
     #assembly_name=canu
     #assembly_name=flye_unpol
     #assembly_name=trinity2
     assembly_name=canu_unpol
 
-#Specify directory structure and create them
-    course_dir=/data/users/xdeng/assembly_annotation_course
-        polish_evaluation_dir=${course_dir}/polish_eval
-            evaulation_dir=${polish_evaluation_dir}/eval
-                BUSCO_dir=${evaulation_dir}/BUSCO
-                    assembly_BUSCO_dir=${BUSCO_dir}/${assembly_name}
-    
-    #mkdir ${evaulation_dir}
-    #mkdir ${BUSCO_dir}
-    #mkdir ${assembly_BUSCO_dir}
+#structure specification
+course_dir=/data/users/xdeng/assembly_annotation_course
+polish_evaluation_dir=${course_dir}/polish_eval
+evaulation_dir=${polish_evaluation_dir}/eval
+BUSCO_dir=${evaulation_dir}/BUSCO
+assembly_BUSCO_dir=${BUSCO_dir}/${assembly_name}
+
 # --------------------------------------------------------------------------------------------
-#Specify the assembly to use (!!!COMMENT OUT THE ONE YOU ARE NOT USING!!!)
+#Inoput
     #assembly=${polish_evaluation_dir}/polish/pilon/canu/canu.fasta
     assembly=${course_dir}/canu_3/pacbio_canu_3.contigs.fasta #Unpolished canu assembly
     #assembly=${polish_evaluation_dir}/polish/pilon/flye/flye.fasta
     #assembly=${course_dir}/flye/assembly.fasta #Unpolished flye assembly
     #assembly=${course_dir}/rnaseq_assembly/trinity2/Trinity.fasta
 
-#Go to folder where the evaluation results will be stored
+#Go to the folder where the evaluation results will be stored
     cd ${assembly_BUSCO_dir}
 
 #Make a copy of the augustus config directory to a location where I have write permission
@@ -47,9 +44,9 @@
 #Run busco to assess the quality of the assemblies
     busco -i ${assembly} -l brassicales_odb10 -o ${assembly_name} -m genome --cpu 8
         #Options entered here are:
-            #"-i": Input sequence file in FASTA format. Can be an assembled genome or transcriptome (DNA), or protein sequences from an annotated gene set.
-            #"-l": Specify the name of the BUSCO lineage to be used.
-            #"-o": defines the folder that will contain all results, logs, and intermediate data
+            #"-i": input file for BUSCO to analyze
+            #"-l": define the specific BUSCO lineage dataset that will be used for the assessment. BUSCO lineages are curated sets of highly conserved genes expected to be found in all members of a taxonomic group
+            #"-o": This option sets the name for the output directory where BUSCO will save all the results, logs, and intermediate files generated during the analysis. 
             #"-m": Specify which BUSCO analysis mode to run, genome, proteins, transcriptome (!!!FOR THE CANU AND FLYE ASSEMBLY I USE GENOME AND FOR THE TRINITRY ASSEMBLY I USE TRANSCRIPTOME!!!)
             #"--cpu": Specify the number (N=integer) of threads/cores to use.
 
