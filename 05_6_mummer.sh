db1a3158084ecef5b19452811e241b2894d1ea90
Lenax_D <122524295+XiaoyueLenax@users.noreply.github.com>
2024-01-31 18:14:49 +0100

    Update 05_6_mummer.sh

--- 05_6_mummer.sh
+++ 05_6_mummer.sh
@@ -12,6 +12,7 @@
 module add UHTS/Analysis/mummer/4.0.0beta1
 export PATH=/software/bin:$PATH
 
+#Inputs
 reference_file="/data/users/xdeng/assembly_annotation_course/raw_data/references/Arabidopsis_thaliana.TAIR10.dna.toplevel.fa"
 #reference_file="/data/users/xdeng/assembly_annotation_course/polish_eval/polish/pilon/flye/flye.fasta"
 #reference_file="/data/users/xdeng/assembly_annotation_course/polish_eval/polish/pilon/canu/canu.fasta"
@@ -22,6 +23,7 @@ reference_file="/data/users/xdeng/assembly_annotation_course/raw_data/references
 #query_file="/data/users/xdeng/assembly_annotation_course/polish_eval/polish/pilon/canu/canu.fasta"
 query_file="/data/users/xdeng/assembly_annotation_course/polish_eval/polish/pilon/flye/flye.fasta"
 
+#Task ID for tracking purposes
 #task_name=flye
 #task_name=canu
 #task_name=flye_pol
@@ -41,4 +43,14 @@ match_file=/data/users/xdeng/assembly_annotation_course/comparison/mummer/try2/f
 #match_file=/data/users/xdeng/assembly_annotation_course/comparison/mummer/try2/compar2.delta
 
 mummerplot -filter -l -R ${reference_file} -Q ${query_file} --large --png -p $task_name --layout --fat ${match_file}
-
+# Options explained:
+  #-filter: Filters out alignments, improving clarity by removing clutter.
+  #-l: Forces the use of the 'large' layout for plotting, suitable for large genomes.
+  #-R ${reference_file}: Specifies the reference genome for labelling the plot.
+  #-Q ${query_file}: Specifies the query genome for labelling the plot.
+  #--large: Optimizes plot settings for large datasets.
+  #--png: Outputs the plot in PNG format.
+  #-p $task_name: Sets the prefix for the output files, defined by $task_name.
+  #--layout: Uses an improved layout algorithm for the plot.
+  #--fat: Increases the line width in the plot, making it easier to see.
+  #${match_file}: Specifies the input file containing the alignment data to be plotted.
