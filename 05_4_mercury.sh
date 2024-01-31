e0b964e9d962472f6fe7fd5066884f9ab742adf9
Lenax_D <122524295+XiaoyueLenax@users.noreply.github.com>
2024-01-31 17:57:22 +0100

    Update 05_4_mercury.sh

--- 05_4_mercury.sh
+++ 05_4_mercury.sh
@@ -18,16 +18,16 @@
 assembly_name=canu_unpol
 
 #Specify directory structure and create them
-    course_dir=/data/users/xdeng/assembly_annotation_course
-        raw_data_dir=${course_dir}/raw_data
-        polish_evaluation_dir=${course_dir}/polish_eval
-            evaulation_dir=${polish_evaluation_dir}/eval
-            #evaulation_dir=${polish_evaluation_dir}/no_polish
-                meryl_dir=${evaulation_dir}/meryl
-                #meryl_dir=${polish_evaluation_dir}/no_polish
-                merqury_dir=${evaulation_dir}/merqury
-                    assembly_merqury_dir=${merqury_dir}/${assembly_name}
-                    assembly_meryl_dir=${meryl_dir}/${assembly_name}
+course_dir=/data/users/xdeng/assembly_annotation_course
+raw_data_dir=${course_dir}/raw_data
+polish_evaluation_dir=${course_dir}/polish_eval
+evaulation_dir=${polish_evaluation_dir}/eval
+#evaulation_dir=${polish_evaluation_dir}/no_polish
+meryl_dir=${evaulation_dir}/meryl
+#meryl_dir=${polish_evaluation_dir}/no_polish
+merqury_dir=${evaulation_dir}/merqury
+assembly_merqury_dir=${merqury_dir}/${assembly_name}
+assembly_meryl_dir=${meryl_dir}/${assembly_name}
     
 #Specify the assembly to use
     #assembly=${polish_evaluation_dir}/polish/pilon/canu/canu.fasta #Polished canu assembly
@@ -35,11 +35,11 @@ assembly_name=canu_unpol
     #assembly=${polish_evaluation_dir}/polish/pilon/flye/flye.fasta #Polished flye assembly
     #assembly=${course_dir}/flye/assembly.fasta #Unpolished flye assembly
 
-#Change permisson of assembly otherwise there is an error (I did not fully understand why) and go to folder where results should be stored.
-    chmod ugo+rwx ${assembly}
-    cd ${assembly_merqury_dir}
+#Change permission of assembly otherwise there is an error (I did not fully understand why) and go to folder where results should be stored.
+chmod ugo+rwx ${assembly}
+cd ${assembly_merqury_dir}
 
-#Run merqury to assess quality of the assemblies; do not indent
+#Run merqury to assess the quality of the assemblies; do not indent
 apptainer exec \
 --bind $course_dir \
 /software/singularity/containers/Merqury-1.3-1.ubuntu20.sif \
